f64c574eb28f1614239b734fbdf9d459366cb39c
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-06-26 19:36:34 +0530

    minor date=date-1 update

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # app.py
 
 import streamlit as st
-from datetime import datetime
+from datetime import datetime,timedelta
 from langchain_community.document_loaders import AsyncHtmlLoader
 from langchain_community.document_transformers import Html2TextTransformer
 import google.generativeai as genai
-today = datetime.today().strftime('%d-%m-%Y')
+yesterday = (datetime.today() - timedelta(days=1)).strftime('%d-%m-%Y')
 def generate_dynamic_url():
     
-    today = datetime.today().strftime('%d-%m-%Y')
-    url = f'https://www.drishtiias.com/current-affairs-news-analysis-editorials/news-analysis/{today}'
+    yesterday = (datetime.today() - timedelta(days=1)).strftime('%d-%m-%Y')
+    url = f'https://www.drishtiias.com/current-affairs-news-analysis-editorials/news-analysis/{yesterday}'
     return url
 
 def generate_mcqs():
@@ -27,7 +27,7 @@ def generate_mcqs():
     GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-1.5-pro')
-    query = "Act as an MCQ Generator. Generate 15 MCQs based on the provided context. Ignore unnecessary HTML content and focus on text starting from 'n min read' to 'Infographics'. Exclude all other content. Always Give Answer key in end" + "content" + str(docs_transformed)
+    query = "Act as an MCQ Generator. Generate 15 MCQs based on the provided context. Ignore unnecessary HTML content and focus on text starting from 'n min read' to 'Infographics'. Exclude all other content. Always Give Answer key in end" + "content" + str(docs_transformed)+"Start With Here are questions you need to practice"
     
     # Generate MCQs
     response = model.generate_content(query)
@@ -35,7 +35,8 @@ def generate_mcqs():
     return response.text
 
 def main():
-    st.title(str('UPSC MCQ Generator for today i.e ')+str(today))
+    st.title(str('UPSC MCQ Generator for today i.e ')+str(yesterday))
+    st.write("This returns MCQ for yesterday")
     
     # Generate MCQs button
     if st.button('Generate MCQs for Today\'s Current Affairs'):
