1d6a619da537196a094c64d3682fb79981cad254
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-06-26 16:03:38 +0530

    v1 basic poc

---
+++ app.py
@@ -0,0 +1,47 @@
+# app.py
+
+import streamlit as st
+from datetime import datetime
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_community.document_transformers import Html2TextTransformer
+import google.generativeai as genai
+today = datetime.today().strftime('%d-%m-%Y')
+def generate_dynamic_url():
+    
+    today = datetime.today().strftime('%d-%m-%Y')
+    url = f'https://www.drishtiias.com/current-affairs-news-analysis-editorials/news-analysis/{today}'
+    return url
+
+def generate_mcqs():
+    url = generate_dynamic_url()
+    
+    # Load HTML
+    loader = AsyncHtmlLoader([url])
+    docs = loader.load()
+    
+    # Transform HTML to text
+    html2text = Html2TextTransformer()
+    docs_transformed = html2text.transform_documents(docs)
+    
+    # Configure and use GenerativeAI model
+    GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    query = "Act as an MCQ Generator. Generate 15 MCQs based on the provided context. Ignore unnecessary HTML content and focus on text starting from 'n min read' to 'Infographics'. Exclude all other content. Always Give Answer key in end" + "content" + str(docs_transformed)
+    
+    # Generate MCQs
+    response = model.generate_content(query)
+    
+    return response.text
+
+def main():
+    st.title(str('UPSC MCQ Generator for today i.e ')+str(today))
+    
+    # Generate MCQs button
+    if st.button('Generate MCQs for Today\'s Current Affairs'):
+        mcqs = generate_mcqs()
+        st.subheader('Generated MCQs:')
+        st.write(mcqs)
+
+if __name__ == '__main__':
+    main()
