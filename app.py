4669e5bbfdf4628495403270214e10b4602b8488
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-06-30 10:54:36 +0530

    prodcut dev started

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ Comments  -
 '''
 
 import streamlit as st
-st.title("Welcome to Government Exam GK MCQ Practice Zone 	:heart: ")
+st.title("GKMCQ.AI")
+st.write("Welcome to Government Exam GK MCQ Practice Zone 	:heart: ")
 st.sidebar.header("Made by Aditya Bhatt")
 st.sidebar.image("https://saibaba9758140479.blob.core.windows.net/docs/pic_me.PNG")
 st.sidebar.markdown("[Linkedin](https://www.linkedin.com/in/adityaabhatt/)")
 st.sidebar.markdown("[Website](https://aiwithaditya.odoo.com/blog)")
 st.sidebar.markdown("[Instagram](https://www.instagram.com/your_data_scientist/)")
+
+
 
 
 
